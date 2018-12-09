11de3b55534bb4e1836e18d1fc9916403f22946c
danilobrinu <ddbn.c2@gmail.com>
2018-12-09 12:47:41 -0500

    Fix warning: Type "Node" is missing a "resolveType" resolver

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server")
-const { Prisma } = require("prisma-binding")
-const { importSchema } = require("graphql-import")
-const resolvers = require("./resolvers")
+const { ApolloServer, makeExecutableSchema } = require('apollo-server');
+const { Prisma } = require('prisma-binding');
+const { importSchema } = require('graphql-import');
+
+const typeDefs = importSchema('./src/schema.graphql');
+const resolvers = require('./resolvers');
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false,
+  },
+});
 
 const server = new ApolloServer({
-  typeDefs: importSchema("./src/schema.graphql"),
-  resolvers,
+  schema,
   context: req => ({
     ...req,
     db: new Prisma({
-      typeDefs: "./src/generated/prisma.graphql", // the auto-generated GraphQL schema of the Prisma API
+      typeDefs: './src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API
       debug: true, // log all GraphQL queries & mutations sent to the Prisma API
       // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml`
     }),
   }),
-})
+});
 
-server.listen().then(({ url }) => `🚀  Server ready at ${url}`)
+server.listen().then(({ url }) => `🚀  Server ready at ${url}`);
